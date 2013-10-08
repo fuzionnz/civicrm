823bbc3759e8def5dce2d38566d4819b7e716cc1
Eileen <eileen@fuzion.co.nz>
2013-10-09 09:55:51 +1300

    Revert "CRM-13379 js, contribution form - generalise  button double submit protection and add to contribution.main to prevent double submits where people have no confirm page"
    This reverts commit 7946e27e9b6be7f4659d29ca667f7decce87761c.

--- templates/CRM/Core/Form.js
+++
@@ -1,8 +0,0 @@
-cj(function ($) {
-    'use strict';
-    $('.form-submit').on("click", function() {
-      $('.form-submit').attr({value: ts('Processing'), disabled : 'disabled'});
-      $('.crm-form-button-back ').closest('span').hide();
-      $('.crm-form-button-cancel').closest('span').hide();
-    });
-});

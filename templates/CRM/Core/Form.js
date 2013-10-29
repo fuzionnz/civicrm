8bbb450de92116d87e09e8caab520ed965573670
Jonathan Hunt <git@huntdesign.co.nz>
2013-10-30 09:37:44 +1300

    add next button fix for double submit protection

--- templates/CRM/Core/Form.js
+++ templates/CRM/Core/Form.js
@@ -8,7 +8,7 @@ cj(function ($) {
   }
   $('.crm-form-button-submit').attr({value: 'Processing'}).closest('form').submit();
   $('.crm-form-button-upload').attr({value: 'Processing'}).closest('form').submit();
-
+  $('.crm-form-button-next').attr({value: 'Processing'}).closest('form').submit();
   $('.crm-form-button-back ').closest('span').hide();
   $('.crm-form-button-cancel').closest('span').hide();
   return false;

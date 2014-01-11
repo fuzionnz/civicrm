CiviCRM42/civicrm42-core

backport 4.4. exception class

CRM/Core/Exception.php
@@ -1,0 +1,58 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | CiviCRM version 4.3                                                |
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC (c) 2004-2013                                |
+ +--------------------------------------------------------------------+
+ | This file is a part of CiviCRM.                                    |
+ |                                                                    |
+ | CiviCRM is free software; you can copy, modify, and distribute it  |
+ | under the terms of the GNU Affero General Public License           |
+ | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
+ |                                                                    |
+ | CiviCRM is distributed in the hope that it will be useful, but     |
+ | WITHOUT ANY WARRANTY; without even the implied warranty of         |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
+ | See the GNU Affero General Public License for more details.        |
+ |                                                                    |
+ | You should have received a copy of the GNU Affero General Public   |
+ | License and the CiviCRM Licensing Exception along                  |
+ | with this program; if not, contact CiviCRM LLC                     |
+ | at info[AT]civicrm[DOT]org. If you have questions about the        |
+ | GNU Affero General Public License or the licensing of CiviCRM,     |
+ | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
+ +--------------------------------------------------------------------+
+*/
+
+/**
+ * Base class for exceptions generated by CiviCRM.
+ * This Exception returns more information than the default one. We are using it from the
+ * form layer to allow redirects to occur without having redirects in the BAO
+ * @param string $message the human friendly error message
+ * @param string $error_code a computer friendly error code. By convention, no space (but underscore allowed)
+ *  ex: mandatory_missing, duplicate, invalid_format
+ * @param array $data extra params to return. eg an extra array of ids. It is not mandatory, but can help the computer using the api. Keep in mind the api consumer isn't to be trusted. eg. the database password is NOT a good extra data
+ */
+
+class CRM_Core_Exception extends PEAR_Exception {
+  private $errorData = array();
+  public function __construct($message, $error_code = 0, $errorData = array(), $previous = null) {
+    parent::__construct(ts($message));
+    $this->errorData = $errorData + array('error_code' => $error_code);
+  }
+
+  // custom string representation of object
+  public function __toString() {
+    return __CLASS__ . ": [{$this->errorData['error_code']}: {$this->message}\n";
+  }
+
+  public function getErrorCode() {
+    return $this->errorData['error_code'];
+  }
+  public function getErrorData() {
+    return $this->errorData;
+  }
+}
+

fuzionnz/civicrm

Backport 4.4 api exception class

api/Exception.php
@@ -68,3 +68,32 @@ class CiviCRM_API3_Exception extends Exception
     return $this->extraParams;
   }
 }
+
+/**
+ * This api exception returns more information than the default one. We are using it rather than
+ * API_Exception from the api wrapper as the namespace is more generic
+ * @param string $message the human friendly error message
+ * @param string $error_code a computer friendly error code. By convention, no space (but underscore allowed)
+ *  ex: mandatory_missing, duplicate, invalid_format
+ * @param array $data extra params to return. eg an extra array of ids. It is not mandatory, but can help the computer using the api. Keep in mind the api consumer isn't to be trusted. eg. the database password is NOT a good extra data
+ */
+class CiviCRM_API3_Exception extends Exception
+{
+  private $extraParams = array();
+  public function __construct($message, $error_code, $extraParams = array(),Exception $previous = null) {
+    parent::__construct(ts($message));
+    $this->extraParams = $extraParams + array('error_code' => $error_code);
+  }
+
+  // custom string representation of object
+  public function __toString() {
+    return __CLASS__ . ": [{$this->extraParams['error_code']}: {$this->message}\n";
+  }
+
+  public function getErrorCode() {
+    return $this->extraParams['error_code'];
+  }
+  public function getExtraParams() {
+    return $this->extraParams;
+  }
+}
7b0761032e7ab3ec2d5975b138714057481e5616
Eileen McNaughton <eileen@mcnaughty.com>
2014-07-03 11:15:36 +1200

    Update civicrm-version.php

--- civicrm-version.php
+++ civicrm-version.php
@@ -1,6 +1,6 @@
 <?php
 function civicrmVersion( ) {
-  return array( 'version'  => '4.2.15',
+  return array( 'version'  => '4.2.17',
                 'cms'      => 'Drupal',
                 'revision' => 'de7be86a30' );
 }
